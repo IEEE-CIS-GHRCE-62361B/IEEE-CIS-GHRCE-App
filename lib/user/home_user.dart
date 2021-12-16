639dabfe8a350d58ffa139259dd4bf59a81e19d2
Vedant Bahel <vedant556@gmail.com>
2021-12-16 14:14:44 +0530

    home_page_user

--- lib/user/home_user.dart
+++ lib/user/home_user.dart
@@ -6,15 +6,97 @@ class home_user extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        title: Text("IEEE CIS - GHRCE"),
+        title: Text("Home"),
       ),
       body: Center(
-        child: Container(
-          child: Text("IEEE CIS GHRCE"),
-        ),
+        child: Column(children: [
+          Text(
+            "\n Upcoming Events",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          Card(
+            clipBehavior: Clip.antiAlias,
+            child: Column(
+              children: [
+                ListTile(
+                  leading: Icon(Icons.announcement_rounded, color: Colors.pink),
+                  title: const Text('AI Summit 2022'),
+                  subtitle: Text(
+                    '10th & 11th Jan 2022',
+                    style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    'Greyhound divisively hello coldly wonderfully marginally far upon excluding.',
+                    style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                  ),
+                ),
+                ButtonBar(
+                  alignment: MainAxisAlignment.start,
+                  children: [
+                    FlatButton(
+                      textColor: const Color(0xFF6200EE),
+                      onPressed: () {
+                        // Perform some action
+                      },
+                      child: const Text('Register'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Card(
+            clipBehavior: Clip.antiAlias,
+            child: Column(
+              children: [
+                ListTile(
+                  leading: Icon(Icons.announcement_rounded, color: Colors.pink),
+                  title: const Text('AI Summit 2022'),
+                  subtitle: Text(
+                    '10th & 11th Jan 2022',
+                    style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    'Greyhound divisively hello coldly wonderfully marginally far upon excluding.',
+                    style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                  ),
+                ),
+                ButtonBar(
+                  alignment: MainAxisAlignment.start,
+                  children: [
+                    FlatButton(
+                      textColor: const Color(0xFF6200EE),
+                      onPressed: () {
+                        // Perform some action
+                      },
+                      child: const Text('Register'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ]),
       ),
-      drawer: Drawer(),
+      drawer: Drawer(
+          child: ListView(padding: EdgeInsets.zero, children: [
+        const DrawerHeader(
+          decoration: BoxDecoration(
+            color: Colors.blue,
+          ),
+          child: Text('IEEE CIS GHRCE'),
+        ),
+        ListTile(title: const Text('Item 1'), onTap: () {}),
+        ListTile(title: const Text('Item 1'), onTap: () {}),
+      ])),
     );
   }
 }
